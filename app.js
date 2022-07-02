b1050a43d275f4ebbb40c2eedaf0f8e69c2ed654
mmerchut <55309412+mmerchut@users.noreply.github.com>
2022-07-02 17:05:28 +0200

    develop

--- app.js
+++ app.js
@@ -7,23 +7,46 @@ function changePosition(e) {
     x = e.which;
     y = square.style;
     if(x == 40) {
-        marginT+=2;
-        y.marginTop = `${marginT}px`;
+        marginT+=4;
+        y.top = `${marginT}px`;
     }
     if(x == 39) {
-        marginR+=2;
-        y.marginLeft = `${marginR}px`;
+        marginR+=4;
+        y.left = `${marginR}px`;
     }
     if(x == 38) {
-        marginT-=2;
-        y.marginTop = `${marginT}px`;
+        marginT-=4;
+        y.top = `${marginT}px`;
     }
     if(x == 37) {
-        marginR-=2;
-        y.marginLeft = `${marginR}px`;
+        marginR-=4;
+        y.left = `${marginR}px`;
     }
+    checkForShop()
 }
 
+function checkForShop() {
+    if(marginR == 20){
+        shop();
+    }
+    
+}
+
+function shop() {
+    console.log("shop");
+}
+
+function checkForShop() {
+    if(marginR >= 119 || marginR >= 119 & marginR <= 122){
+     fight();
+    }
+    
+}
+
+function fight() {
+    console.log("fight");
+    alert('fight');
+}
 
 
 
